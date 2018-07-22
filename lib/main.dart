6e1b8ac6f92ce6eef50012a4f2aaaeadd94ff310
was.li <wasuwat.se@gmail.com>
2018-07-22 11:08:19 +0700

    [➕] import http lib [✨] fecthes profile from gh api

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() => runApp(new MyApp());
 
@@ -10,22 +13,70 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
-      home: new MyHomePage(title: 'My Github Profile'),
+      home: new MyGithubProfilePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
+class MyGithubProfilePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  _MyGithubProfileState createState() => new _MyGithubProfileState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyGithubProfileState extends State<MyGithubProfilePage> {
+  final String url = "https://api.github.com/users/rappad";
+  Object profileJSON;
+  Profile profile;
+
+  Future<String> getJSONData() async {
+    var response = await http
+        .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+    setState(() {
+      var dataConvertedToJSON = JSON.decode(response.body);
+      profileJSON = dataConvertedToJSON;
+      profile = Profile.fromJson(profileJSON);
+      print(profile.name);
+    });
+    return "Successfull";
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this.getJSONData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Github profile"),
+      ),
+      body: _buildProfile(),
+    );
+  }
+
+  Widget _buildProfile() {
+    return new Container(
+      child: new Center(
+        child: Text(profile.name),
+      ),
+    );
+  }
+}
+
+class Profile {
+  final String name;
+  final String location;
+
+  Profile({
+    this.name,
+    // this.location,
+  });
+  factory Profile.fromJson(Map<String, dynamic> json) {
+    return Profile(
+      name: json['name'] as String,
+      // location: json['location'] as String,
+    );
   }
 }
